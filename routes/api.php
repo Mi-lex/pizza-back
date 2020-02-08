11be88140ae7fbb94aa32ce05e9fd50d3ec98490
Mi-lex <mishin-lex@mail.ru>
2020-02-08 21:00:02 +0300

    make api for getting menu items

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,10 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'prefix' => 'menu_items'
+], function () {
+    Route::get('/', 'MenuItemController@list');
+    Route::get('types', 'TypeController@list'); 
+    Route::get('/{type}', 'MenuItemsController@listOfType');
 });
